362b2b13e339532dbcc12500caef47b880c39054
Paraphnchai <paraphornchai1995@gml.com>
2025-11-25 10:37:34 +0700

    first coit

---
+++ code.js
@@ -0,0 +1,57 @@
+// var score = 40;
+// if(score <= 100 && score >= 80){
+//     console.log("A");
+// } else if (score < 80 && score >= 70){
+//     console.log("B");
+// } else if (score < 70 && score >= 60){
+//     console.log("C");
+// } else if (score < 60 && score >= 50){
+//     console.log("D");
+// } else if (score < 50){
+//     console.log("F");
+// }
+
+
+// var height = 173;
+// var weight = 65;
+
+// var bmi = weight /( height/100)**2
+
+
+// console.log(bmi);
+
+// let myName = "Paraphornchai";
+// myName = myName.slice(0,3);
+// console.log(myName);
+
+// let news = prompt("What's news today?");
+// let newsToday = news.slice(0,140);
+// alert("News Today: " + newsToday);
+
+// let myName = prompt("What's your name?");
+// let firstChar = myName.slice(0,1);
+// let firstCharUpper = firstChar.toUpperCase();
+// let otherChar = myName.slice(1,myName.length);
+// let otherCharLower = otherChar.toLowerCase();
+// let fullName = firstCharUpper + otherCharLower;
+// alert("Hello " + fullName);
+
+// let dogYears = prompt("Enter your dog's age in human years:");
+// let humanYearsToDogYears = ((dogYears - 2) * 4) + 21;
+// alert("your dog is " + humanYearsToDogYears + " in dog years");
+
+function buyTea(money) {
+   console.log("move");
+   console.log("move");
+   console.log("move");
+
+   let priceTea = Math.floor(money / 19.9);
+
+   console.log("buy tea " + priceTea + " cups of tea");
+   console.log("move");
+   console.log("move");
+   console.log("move");
+
+}
+
+buyTea(100);
